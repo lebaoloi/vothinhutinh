dffb11614b7fe9db29524255ee8ddd874af25a7f
lebaoloi <128689450+lebaoloi@users.noreply.github.com>
2024-10-19 17:22:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = 'https://www.facebook.com/letranbaoloi/';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
@@ -8,6 +8,6 @@ const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và nh�
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'girl_friend.jpeg';
+const giftImage = 'IMG_0127.PNG';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
