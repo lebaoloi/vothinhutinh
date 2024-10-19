a855161241846dbd7295be6197646c3c5bf48d27
lebaoloi <128689450+lebaoloi@users.noreply.github.com>
2024-10-19 17:57:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@
 const nameGirl = 'Em';
 const giftUrl = 'https://www.facebook.com/letranbaoloi/';
 const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy h';
-const contentCard = 'huhu';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Anh không biết định nghĩa tình yêu là gì nhưng cảm giác mỗi khi thức giấc lại nhớ đến em đầu tiên và cảm giác mỗi khi đi ngủ lại chỉ nghĩ về em, khiến anh nhận ra em chính là tình yêu của anh.Chúc honey của anh 20/10 tràn ngập niềm vui và nụ cười luôn nở trên môi.Hãy luôn nhớ rằng, em là một người không thể thiếu trong cuộc đời anh.';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 const giftImage = 'IMG_0127.PNG';
